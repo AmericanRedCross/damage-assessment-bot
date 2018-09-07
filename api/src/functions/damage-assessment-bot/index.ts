99f287c8a07e590e071884c6c6167f9cfbb0cf65
Jituri, Pranav <Pranav.Jituri@appliedis.com>
2018-09-08 01:02:42 +0530

    Form information collection

--- api/src/functions/damage-assessment-bot/index.ts
+++ api/src/functions/damage-assessment-bot/index.ts
@@ -1,8 +1,7 @@
-import { ChatConnector, UniversalBot, Middleware, Prompts, Session, LuisRecognizer, TextFormat } from "botbuilder";
+import { ChatConnector, UniversalBot, Prompts, Session, LuisRecognizer, TextFormat, ListStyle } from "botbuilder";
 import {MongoClient, MongoError} from "mongodb";
 import {MongoBotStorage} from "botbuilder-storage";
 import * as gena from "./dialogs/ask_iana_details";
-import { Dictionary } from "async";
 
 const connector: ChatConnector = new ChatConnector({
     appId: process.env.MicrosoftAppId,
@@ -18,6 +17,8 @@ const mongoDbHostUri:string = process.env.MongoDbHostUri;
 const mongoDbUsername:string = process.env.MongoDbUsername;
 const mongodbPassword:string = process.env.MongoDbPassword;
 
+const MongoDbTtlInSeconds:number = parseInt(process.env.MongoDbTtlInSeconds,10);
+
 MongoClient.connect(mongoDbHostUri,
     {useNewUrlParser: true,auth:{user:mongoDbUsername,password:mongodbPassword}},
     function (err:MongoError,client:MongoClient): any {
@@ -27,9 +28,9 @@ MongoClient.connect(mongoDbHostUri,
         const connectionSettings: any = {
              collection: process.env.MongoDbBotStateCollectionName,
              ttl: {
-                 userData: process.env.MongoDbTtlInSeconds,
-                 conversationData: process.env.MongoDbTtlInSeconds,
-                 privateConversationData: process.env.MongoDbTtlInSeconds
+                 userData: MongoDbTtlInSeconds,
+                 conversationData: MongoDbTtlInSeconds,
+                 privateConversationData: MongoDbTtlInSeconds
              }
    };
    const db:any = client.db(process.env.MongoDbName);
@@ -42,22 +43,28 @@ const recognizer:any = new LuisRecognizer(process.env.LuisConnectionString);
 rcdaBot.recognizer(recognizer);
 
 console.log(recognizer);
-rcdaBot.dialog("GreetingDialog",
-    function (session:Session):any {
-        session.send("You have reached the Greeting intent. You said %s",session.message.text);
-        session.sendTyping();
-        session.endDialog("Ending this conversation :'(");
+rcdaBot.dialog("GreetingDialog",[
+    function (session:Session):void {
+        // session.send("You have reached the Greeting intent. You said %s",session.message.text);
+        // session.sendTyping();
+        // session.endDialog("Ending this conversation :'(");
+        session.send("Testing feature...");
+        Prompts.confirm(session,"Select Yes,No",{listStyle:ListStyle.button});
+    },
+    function (session:Session,results:any):void {
+        console.log(results.response);
+        session.send(`You typed in -- ${results.response.toString()}`);
     }
-).triggerAction({
-    matches: "Greeting"
+]).triggerAction({
+    matches: "HelloWorld"
 });
 
-//rcdaBot.dialog("/",function (session:Session):any {
+// rcdaBot.dialog("/",function (session:Session):any {
 //    session.sendTyping();
 //    session.send("Hello! Your name is %s?",session.userData.Name);
 //    session.sendTyping();
 //    session.endConversation("Thank you for passing the data!");
-//});
+// });
 
 // rcdaBot.use(Middleware.firstRun({ version: 1.0, dialogId: "*:/name" }));
 
@@ -75,10 +82,14 @@ rcdaBot.dialog("/name",[
         session.replaceDialog("/ask_iana");
     }
 ]
-);
+).triggerAction({
+    matches: "Greeting"
+});
 
+let concernSectors:Array<string> = new Array("Health","Food","Wash","Shelter/NFI","Protection","Education","Livelihood","Other");
 let genaFormData:Map<string,string> = new Map();
-let sectorProblem:Map<string,boolean> = new Map();
+let sectorConcern:Map<string,boolean> = new Map();
+
 
 rcdaBot.dialog("/ask_iana",gena.ask_iana_details);
 
@@ -157,7 +168,15 @@ rcdaBot.dialog("/concern_in_sectors",[
         if (confirmation.toLowerCase() === "yes") {
             session.beginDialog("/ask_sector_concerns");
         } else {
-            sectorProblem.set();
+            sectorConcern.set("health",false);
+            sectorConcern.set("food",false);
+            sectorConcern.set("wash",false);
+            sectorConcern.set("shelter",false);
+            sectorConcern.set("protection",false);
+            sectorConcern.set("education",false);
+            sectorConcern.set("livelihood",false);
+            sectorConcern.set("other",false);
+            session.conversationData.sectorConcern = sectorConcern;
         }
     }
 ]);
@@ -169,65 +188,155 @@ rcdaBot.dialog("/ask_sector_concerns",[
     function (session:Session,results:any):void {
         const confirmation:string = results.response.toString();
         if (confirmation.toLowerCase() === "yes") {
-            sectorProblem.set("health_sector_concern",true);
+            sectorConcern.set("health",true);
         } else {
-            sectorProblem.set("health_sector_concern",false);
+            sectorConcern.set("health",false);
         }
         Prompts.confirm(session,"Do you have a concern in **FOOD** sector?",{textFormat:TextFormat.markdown});
     },
     function (session:Session,results:any):void {
         const confirmation:string = results.response.toString();
         if (confirmation.toLowerCase() === "yes") {
-            sectorProblem.set("food_sector_concern",true);
+            sectorConcern.set("food",true);
         } else {
-            sectorProblem.set("food_sector_concern",false);
+            sectorConcern.set("food",false);
         }
         Prompts.confirm(session,"Do you have a concern in **WASH** sector?",{textFormat:TextFormat.markdown});
     },
     function (session:Session,results:any):void {
         const confirmation:string = results.response.toString();
         if (confirmation.toLowerCase() === "yes") {
-            sectorProblem.set("wash_sector_concern",true);
+            sectorConcern.set("wash",true);
         } else {
-            sectorProblem.set("wash_sector_concern",false);
+            sectorConcern.set("wash",false);
         }
         Prompts.confirm(session,"Do you have a concern in **SHELTER/NFI** sector?",{textFormat:TextFormat.markdown});
     },
     function (session:Session,results:any):void {
         const confirmation:string = results.response.toString();
         if (confirmation.toLowerCase() === "yes") {
-            sectorProblem.set("shelter_sector_concern",true);
+            sectorConcern.set("shelter",true);
         } else {
-            sectorProblem.set("shelter_sector_concern",false);
+            sectorConcern.set("shelter",false);
         }
         Prompts.confirm(session,"Do you have a concern in **PROTECTION** sector?",{textFormat:TextFormat.markdown});
     },
     function (session:Session,results:any):void {
         const confirmation:string = results.response.toString();
         if (confirmation.toLowerCase() === "yes") {
-            sectorProblem.set("protection_sector_concern",true);
+            sectorConcern.set("protection",true);
         } else {
-            sectorProblem.set("protection_sector_concern",false);
+            sectorConcern.set("protection",false);
         }
         Prompts.confirm(session,"Do you have a concern in **EDUCATION** sector?",{textFormat:TextFormat.markdown});
     },
     function (session:Session,results:any):void {
         const confirmation:string = results.response.toString();
         if (confirmation.toLowerCase() === "yes") {
-            sectorProblem.set("education_sector_concern",true);
+            sectorConcern.set("education",true);
         } else {
-            sectorProblem.set("education_sector_concern",false);
+            sectorConcern.set("education",false);
         }
         Prompts.confirm(session,"Do you have a concern in **LIVELIHOOD** sector?",{textFormat:TextFormat.markdown});
     },
     function (session:Session,results:any):void {
         const confirmation:string = results.response.toString();
         if (confirmation.toLowerCase() === "yes") {
-            sectorProblem.set("livelihood_sector_concern",true);
+            sectorConcern.set("livelihood",true);
         } else {
-            sectorProblem.set("livelihood_sector_concern",false);
+            sectorConcern.set("livelihood",false);
         }
         Prompts.confirm(session,"Do you have a concern in **OTHER** sector ?",{textFormat:TextFormat.markdown});
+    },
+    function (session:Session,results:any):void {
+        const confirmation:string = results.response.toString();
+        if (confirmation.toLowerCase() === "yes") {
+            sectorConcern.set("other",true);
+        } else {
+            sectorConcern.set("other",false);
+        }
+        for (const key in sectorConcern.keys()) {
+            if (sectorConcern.get(key)) {
+                session.beginDialog("/ask_sector_concerns_score");
+                session.beginDialog("/ask_sector_concerns_factors",{sectorConcern:sectorConcern});
+                break;
+            }
+        }
+    }
+]);
+
+rcdaBot.dialog("/ask_sector_concerns_score",[
+    function (session:Session,next:any):void {
+        session.send("On a rating of 1 (Minor Problem) - 5 (Life Threatening Problem), please enter values");
+        if (sectorConcern[concernSectors[0]]) {
+            Prompts.number(session,`What is the severity of concern in  **${concernSectors[0]}** sector?`,{integerOnly:true});
+        } else {
+            next();
+        }
+    },
+    function (session:Session,results:any,next:any):void {
+        session.send("On a rating of 1 (Minor Problem) - 5 (Life Threatening Problem), please enter values");
+        if (sectorConcern[concernSectors[1]]) {
+            Prompts.number(session,`What is the severity of concern in  **${concernSectors[1]}** sector?`,{integerOnly:true});
+        } else {
+            next();
+        }
+    },
+    function (session:Session,sectorConcern:any,next:any):void {
+        session.send("On a rating of 1 (Minor Problem) - 5 (Life Threatening Problem), please enter values");
+        if (sectorConcern[concernSectors[2]]) {
+            Prompts.number(session,`What is the severity of concern in  **${concernSectors[2]}** sector?`,{integerOnly:true});
+        } else {
+            next();
+        }
+    },
+    function (session:Session,sectorConcern:any,next:any):void {
+        session.send("On a rating of 1 (Minor Problem) - 5 (Life Threatening Problem), please enter values");
+        if (sectorConcern[concernSectors[3]]) {
+            Prompts.number(session,`What is the severity of concern in  **${concernSectors[3]}** sector?`,{integerOnly:true});
+        } else {
+            next();
+        }
+    },
+    function (session:Session,sectorConcern:any,next:any):void {
+        session.send("On a rating of 1 (Minor Problem) - 5 (Life Threatening Problem), please enter values");
+        if (sectorConcern[concernSectors[4]]) {
+            Prompts.number(session,`What is the severity of concern in  **${concernSectors[4]}** sector?`,{integerOnly:true});
+        } else {
+            next();
+        }
+    },
+    function (session:Session,sectorConcern:any,next:any):void {
+        session.send("On a rating of 1 (Minor Problem) - 5 (Life Threatening Problem), please enter values");
+        if (sectorConcern[concernSectors[5]]) {
+            Prompts.number(session,`What is the severity of concern in  **${concernSectors[5]}** sector?`,{integerOnly:true});
+        } else {
+            next();
+        }
+    },
+    function (session:Session,sectorConcern:any,next:any):void {
+        session.send("On a rating of 1 (Minor Problem) - 5 (Life Threatening Problem), please enter values");
+        if (sectorConcern[concernSectors[6]]) {
+            Prompts.number(session,`What is the severity of concern in  **${concernSectors[6]}** sector?`,{integerOnly:true});
+        } else {
+            next();
+        }
+    },
+    function (session:Session,sectorConcern:any,next:any):void {
+        session.send("On a rating of 1 (Minor Problem) - 5 (Life Threatening Problem), please enter values");
+        if (sectorConcern[concernSectors[7]]) {
+            Prompts.number(session,`What is the severity of concern in  **${concernSectors[7]}** sector?`,{integerOnly:true});
+        } else {
+            next();
+        }
+    },
+    function (session:Session,sectorConcern:any,next:any):void {
+        session.send("On a rating of 1 (Minor Problem) - 5 (Life Threatening Problem), please enter values");
+        if (sectorConcern[concernSectors[8]]) {
+            Prompts.number(session,`What is the severity of concern in  **${concernSectors[8]}** sector?`,{integerOnly:true});
+        } else {
+            next();
+        }
     }
 ]);
 
