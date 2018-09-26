6a42231e966414857003b57c7aa126e341a963ec
Jituri, Pranav <Pranav.Jituri@appliedis.com>
2018-09-26 20:07:05 +0530

    Rewrote the function so that it could be called externally

--- api/src/functions/keep-alive/index.ts
+++
@@ -1,9 +0,0 @@
-module.exports = async function (context:any, myTimer:any) {
-    const timeStamp = new Date().toISOString();
-    
-    if(myTimer.isPastDue)
-    {
-        context.log('Azure Keep Alive function is running late!');
-    }
-    context.log('Azure Keep Alive function is running late!', timeStamp);   
-};
