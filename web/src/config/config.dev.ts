2f429ae4efeb30acb6e785a97a39db1025d82c40
Max North <maxwelldnorth@gmail.com>
2018-11-05 17:13:12 -0500

    update web app config files

--- web/src/config/config.dev.ts
+++ web/src/config/config.dev.ts
@@ -1,5 +1,5 @@
 import RcdaWebAppConfig from "@/config/RcdaWebAppConfig"
 
 export default new RcdaWebAppConfig({
-    rcdaApiHost: "https://est-hum-azf-bot-dev-01.azurewebsites.net"
+    rcdaApiHost: "https://est1-hum-azf-bot-dev-01.azurewebsites.net"
 });
