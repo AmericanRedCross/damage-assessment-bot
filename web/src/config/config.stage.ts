baeb81de754e46d88c18fe00a923164c4250c046
Max North <maxwelldnorth@gmail.com>
2018-11-12 11:36:26 -0500

    Update stage client config

--- web/src/config/config.stage.ts
+++ web/src/config/config.stage.ts
@@ -1,5 +1,5 @@
 import RcdaWebAppConfig from "@/config/RcdaWebAppConfig"
 
 export default new RcdaWebAppConfig({
-    rcdaApiHost: "https://est1-hum-azf-bot-stg-01.azurewebsites.net"
+    rcdaApiHost: "https://est-hum-azf-bot-stg-01.azurewebsites.net"
 });
