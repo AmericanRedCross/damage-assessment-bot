adbdf741ca80b68c717a260587646500f2042891
Max North <maxwelldnorth@gmail.com>
2018-11-17 14:24:20 -0500

    Add myanmar disaster type enum

---
+++ common/src/models/resources/disaster-assessment/enums/MyanmarDisasterTypes.ts
@@ -0,0 +1,15 @@
+export enum MyanmarDisasterTypes {
+    CarAccident = "CarAccident",
+    Cyclone = "Cyclone",
+    Earthquake = "Earthquake",
+    Explosion = "Explosion",
+    Fire = "Fire",
+    Flood = "Flood",
+    IDP = "IDP",
+    Landslide = "Landslide",
+    Other = "Other",
+    Shooting = "Shooting",
+    StrongWind = "StrongWind",
+    Tornado = "Tornado",
+    Wreck = "Wreck"
+}
