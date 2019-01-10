fcde3db91d63fdb259260df27f0d3f31f482fca8
Max North <maxwelldnorth@gmail.com>
2019-01-09 23:59:28 -0500

    Remove custom web config file from source control

--- web/src/config/config.custom.ts
+++
@@ -1,6 +0,0 @@
-import RcdaWebAppConfig from "@/config/RcdaWebAppConfig"
-
-export default new RcdaWebAppConfig({    
-    // rcdaApiHost: "https://rcda-chat-bot-func.azurewebsites.net"
-    rcdaApiHost: "http://localhost:7071" 
-});
